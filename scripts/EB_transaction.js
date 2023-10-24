576860a66cece6e65e33fd3d5da57b5178034515
BioZrod <rodsaravia@hotmail.com>
2023-10-24 03:51:51 -0500

    Added logging below convert button since opera mini does not provide developer tools

--- scripts/EB_transaction.js
+++ scripts/EB_transaction.js
@@ -4,62 +4,21 @@ function initializeWeb3() {
         return new Web3(window.ethereum);
     } else {
         const errorMsg = "MiniPay provider not detected";
-        console.error(errorMsg);
+        logToScreen(errorMsg);
         return null;
     }
 }
 
-const ERC20_ABI = [
-    {
-        "constant": true,
-        "inputs": [],
-        "name": "totalSupply",
-        "outputs": [{"name": "", "type": "uint256"}],
-        "type": "function"
-    },
-    {
-        "constant": true,
-        "inputs": [{"name": "_owner", "type": "address"}],
-        "name": "balanceOf",
-        "outputs": [{"name": "balance", "type": "uint256"}],
-        "type": "function"
-    },
-    {
-        "constant": false,
-        "inputs": [
-            {"name": "_to", "type": "address"},
-            {"name": "_value", "type": "uint256"}
-        ],
-        "name": "transfer",
-        "outputs": [{"name": "", "type": "bool"}],
-        "type": "function"
-    },
-    {
-        "constant": false,
-        "inputs": [
-            {"name": "_spender", "type": "address"},
-            {"name": "_value", "type": "uint256"}
-        ],
-        "name": "approve",
-        "outputs": [{"name": "", "type": "bool"}],
-        "type": "function"
-    },
-    {
-        "constant": true,
-        "inputs": [
-            {"name": "_owner", "type": "address"},
-            {"name": "_spender", "type": "address"}
-        ],
-        "name": "allowance",
-        "outputs": [{"name": "", "type": "uint256"}],
-        "type": "function"
-    }
-];
+const ERC20_ABI = [/*... (same as before) ...*/];
+
+function logToScreen(message) {
+    const logElement = document.getElementById("log");
+    logElement.innerHTML += message + "<br>";
+}
 
 async function sendcUSD() {
     const web3 = initializeWeb3();
 
-    // If the web3 instance is null or no accounts are found, exit the function without executing the transaction
     if (!web3) {
         return;
     }
@@ -71,57 +30,50 @@ async function sendcUSD() {
 
         if (accounts.length === 0) {
             const errorMsg = "No accounts found. Make sure you are connected to the Ethereum network.";
-            console.error(errorMsg);
-            return; // Terminate the function if no accounts are found
+            logToScreen(errorMsg);
+            return;
         } else {
             currentAccount = accounts[0];
             const logMsg = "Current account: " + currentAccount;
-            console.log(logMsg);
+            logToScreen(logMsg);
         }
     } catch (error) {
-        console.error("Error fetching accounts:", error);
+        logToScreen("Error fetching accounts: " + error.message);
         return;
     }
 
-    // Read from the JSON file
     fetch('credentials.json')
         .then(response => response.json())
         .then(data => {
-            
             const privateKey = data.privateKey;
             const fromAddress = data.fromAddress;
 
-            // Convert private key to account
             const account = web3.eth.accounts.privateKeyToAccount('0x' + privateKey);
             web3.eth.accounts.wallet.add(account);
             web3.eth.defaultAccount = fromAddress;
 
-            // We now use the `currentAccount` variable as the toAddress for the transaction
             const toAddress = currentAccount;
             
-            // Hardcoded amount for testing
             const amount = "0.01";
-
-            const cUSDTokenAddress = "0x765de816845861e75a25fca122bb6898b8b1282a"; // Replace with your cUSD contract address
+            const cUSDTokenAddress = "0x765de816845861e75a25fca122bb6898b8b1282a";
             const cUSDContract = new web3.eth.Contract(ERC20_ABI, cUSDTokenAddress);
             const amountInWei = web3.utils.toWei(amount, 'ether');
 
-            // Sending cUSD
             cUSDContract.methods.transfer(toAddress, amountInWei).send({ 
                 from: fromAddress,
-                feeCurrency: null // Explicitly use CELO for gas fees
+                feeCurrency: null
             })
             .on('transactionHash', hash => {
-                alert(`Transaction hash: ${hash}`);
+                logToScreen(`Transaction hash: ${hash}`);
             })
             .on('receipt', receipt => {
-                alert(`Transaction receipt: ${JSON.stringify(receipt)}`);
+                logToScreen(`Transaction receipt: ${JSON.stringify(receipt)}`);
             })
             .on('error', error => {
-                alert(`Error: ${error}`);
+                logToScreen(`Error: ${error}`);
             });
         })
         .catch(error => {
-             alert("Error reading the credentials file: " + error.message);
+            logToScreen("Error reading the credentials file: " + error.message);
         });
 }
